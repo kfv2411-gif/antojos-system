773e57af7bc46693ff08e20fc4d7ef4c03ec7863
Kalena <kfv2411@gmail.com>
2026-08-30 16:54:09 -0400

    Anadir diseno HTML moderno a la pagina de bienvenida

--- facturacion/views.py
+++ facturacion/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("<h1>¡Bienvenida a Antojos by Dulce Delicias!</h1><p>Espacio de trabajo para Brenda y Kalena.</p><p><a href='/admin/'>Ir al Panel de Administración</a></p>")
+    return render(request, 'facturacion/home.html')

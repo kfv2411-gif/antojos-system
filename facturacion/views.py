3c2efd81a70fa852d6ebb246c7f22559db85bbf5
Kalena <kfv2411@gmail.com>
2026-08-30 16:42:03 -0400

    Corregir formato de vista home

--- facturacion/views.py
+++ facturacion/views.py
@@ -1,8 +1,4 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse(
-        "<h1>¡Bienvenida a Antojos by Dulce Delicias!</h1>",
-        "<p>Espacio de trabajo para Brenda y Kalena.</p>",
-        "<p><a href='/admin/'>Ir al Panel de Administración</a></p>"
-    )
+    return HttpResponse("<h1>¡Bienvenida a Antojos by Dulce Delicias!</h1><p>Espacio de trabajo para Brenda y Kalena.</p><p><a href='/admin/'>Ir al Panel de Administración</a></p>")

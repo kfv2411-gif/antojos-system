fc920b8b4d2f60d7b1efec8ed405d5c2dda53d89
Kalena <kfv2411@gmail.com>
2026-08-31 07:40:55 -0400

    Actualizar ruta del logo sin espacios y configuracion estatica

--- antojos_system/settings.py
+++ antojos_system/settings.py
@@ -139,7 +139,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Configuración de archivos estáticos para producción
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
 
